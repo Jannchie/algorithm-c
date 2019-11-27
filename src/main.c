dd1e84ea9003320fa7e71b23963dc1cb13239c46
Jannchie <jannchie@gmail.com>
2019-11-27 21:55:19 +0900

    UPDATE: main.c

--- src/main.c
+++ src/main.c
@@ -13,13 +13,13 @@ int main(int argc, char const *argv[])
     printf("----------- [链    表 - 开始运行] -----------\n");
     testLinklist();
     printf("=========== [排序算法 - 开始运行] ===========\n");
-    // runSort("Heap   Sort", heapSort, len, times);
-    // runSort("Select Sort", selectSort, len, times);
-    // runSort("Bubble Sort", bubbleSort, len, times);
-    // runSort("Insert Sort", insertSort, len, times);
-    // runSort("Quick  Sort", quickSort, len, times);
-    // runSort("Quick3 Sort", quickSortMidian3, len, times);
-    // runSort("Merge  Sort", mergeSort, len, times);
+    runSort("Heap   Sort", heapSort, len, times);
+    runSort("Select Sort", selectSort, len, times);
+    runSort("Bubble Sort", bubbleSort, len, times);
+    runSort("Insert Sort", insertSort, len, times);
+    runSort("Quick  Sort", quickSort, len, times);
+    runSort("Quick3 Sort", quickSortMidian3, len, times);
+    runSort("Merge  Sort", mergeSort, len, times);
     printf("=========== [排序算法 - 运行结束] ===========\n");
     return 0;
 }
