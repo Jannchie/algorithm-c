a2262f00914dff70d5eee8a05533e41dd281c785
Jannchie <jannchie@gmail.com>
2020-06-13 02:49:41 +0900

    Add: Dynamic programming Fibonacci

---
+++ src/dynamic-programming/fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int fib(int N)
+{
+    int p = 0, q = 1, r = 1;
+    while (N--)
+    {
+        p = q;
+        q = r;
+        r = p + q;
+    }
+    return p;
+}
+
+int main(int argc, char const *argv[])
+{
+    for (int n = 0; n <= 32; n++)
+    {
+        printf("Fibonacci number: f(%d) is %d\n", n, fib(n));
+    }
+    return 0;
+}
