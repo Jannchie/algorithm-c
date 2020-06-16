6959e281367f8b6b0200def06e120baf12a656cf
Jannchie <jannchie@gmail.com>
2020-06-16 12:35:56 +0900

    Add: One Line Fib

--- src/fibonacci/recursive-fib.c
+++ src/fibonacci/recursive-fib.c
@@ -20,11 +20,25 @@ int fib(int n)
     result = fib(n - 1) + fib(n - 2);
     return result;
 }
+
+/**
+ * 一行的斐波那契数列递归
+ * 东大 2015 iii-s-3
+ */
+int fib_2(int n)
+{
+
+    return n < 1 ? 0 : n == 1 ? 1 : fib_2(n - 2) + fib_2(n - 1);
+}
+
 int main(int argc, char const *argv[])
 {
     printf("%d\n", fib(10));
     printf("%d\n", fib(20));
     printf("%d\n", fib(30));
     printf("%d\n", fib(40));
+
+    printf("%d\n", fib_2(10));
+    printf("%d\n", fib_2(20));
     return 0;
 }
