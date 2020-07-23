b6f3f66e4da95b426060fcb1b1266ef866067bc1
Jannchie <jannchie@gmail.com>
2020-07-23 16:34:45 +0900

    Add: Return when assigned

---
+++ src/temp/return.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int grant_value(int v)
+{
+    int t = 0;
+    return t = v;
+}
+int main(int argc, char const *argv[])
+{
+    std::cout << ("%d", grant_value(10)) << std::endl;
+    /* code */
+    return 0;
+}
