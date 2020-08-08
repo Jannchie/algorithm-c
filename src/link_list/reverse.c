efdcbf517c510de8c5a7389fc2b84048b532e841
Jannchie <jannchie@gmail.com>
2020-08-08 16:07:09 +0900

    Add: Double head reverse

--- src/link_list/reverse.c
+++ src/link_list/reverse.c
@@ -12,22 +12,41 @@ struct ListNode *reverseList_recursively(struct ListNode *head)
 }
 struct ListNode *reverseList_iteratively(struct ListNode *head)
 {
-    struct ListNode *a = NULL;
-    struct ListNode *b = head;
+    struct ListNode *a = NULL, *b = head, *c;
     while (b != NULL)
     {
-        struct ListNode *c = b->next;
+        c = b->next;
         b->next = a;
         a = b;
         b = c;
     }
     return a;
 }
+
+struct ListNode *double_head_reverse(struct ListNode *head1, struct ListNode *head2)
+{
+    if (head1 == NULL)
+    {
+        return head2;
+    }
+    struct ListNode *new_head1 = head1->next;
+    head1->next = head2;
+    struct ListNode *new_head2 = head1;
+    return double_head_reverse(new_head1, new_head2);
+}
+
 int main(int argc, char const *argv[])
 {
     struct ListNode *head = gen_list(10);
+    printf("Double head reverse: \n");
+    print_list(head);
+    head = double_head_reverse(head, NULL);
+    print_list(head);
+    printf("Iteratively reverseList_: \n");
     print_list(head);
     head = reverseList_iteratively(head);
+    print_list(head);
+    printf("Recursively reverseList_: \n");
     print_list(head);
     head = reverseList_recursively(head);
     print_list(head);
